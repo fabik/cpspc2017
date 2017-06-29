9fc545682d05f6948d92f4e83c8ad769d8c42c1c
Lech Duraj <lech.duraj@gmail.com>
2017-06-29 21:21:42 +0200

    Yet another final version of corpo.

--- tester/corpo/gen.cpp
+++ tester/corpo/gen.cpp
@@ -23,13 +23,13 @@ oi::Random RG ;
 const string TASK = "kor" ;
 
 const int TESTS = 4 ;
-const VI N[TESTS] = { {100, 500, 1000},          // male testy dla bruta kors1
-                      {10000, 15000},            //            dla       kors2
-                      {90000, 100000},
-                      {180000, 190000, 200000} } ;
-const int GROUPSIZE = 3 ;
-const double pathRatio1[GROUPSIZE] = { 0.4, 0.6, 0.8 } ;
-const double pathRatio2[GROUPSIZE] = { 0.3, 0.5, 0.7 } ;
+const VI N[TESTS] = { {100, 1000, 2000},            // male testy dla bruta
+                      {100000, 190000, 200000} } ;  // duze testy dla wzorcowki
+const int PATH_RATIO_SIZE = 3 ;
+const double pathRatio1[PATH_RATIO_SIZE] = { 0.4, 0.6, 0.8 } ;
+const double pathRatio2[PATH_RATIO_SIZE] = { 0.3, 0.5, 0.7 } ;
+
+const int ALMOST_EQUAL_PATH_TYPES = 2 ;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -68,11 +68,11 @@ void genTreesWithSqrtHeightAndWidth(int n, VI &line1, VI &line2) {
         nodes[i] = i ;
     RG.randomShuffle(nodes.begin()+1, nodes.end()) ;
     
-    VI parent2 = parent ;
+    VI parent2(n) ;
     
-    int SQRT = sqrt(n) ;
+    const int SQRT = sqrt(n) ;
     
-     for(int start=0, i=1 ; i<n ; i+=SQRT, start++) {
+    for(int start=0, i=1 ; i<n ; i+=SQRT, start++) {
         parent[nodes[i]] = nodes[start] ;
         for(int j=i+1 ; j<i+SQRT && j<n ; j++)
             parent[nodes[j]] = nodes[j-1] ;
@@ -91,6 +91,47 @@ void genTreesWithSqrtHeightAndWidth(int n, VI &line1, VI &line2) {
         line2.PB(parent2[i]+1) ;
     }
 }
+
+void genAlmostEqualPaths(int n, VI &line1, VI &line2, int type) {
+    // dla wygody wierzcholki numerujemy 0..n-1
+    VI nodes(n), parent(n) ;
+    REP(i, n)
+        nodes[i] = i ;
+    RG.randomShuffle(nodes.begin()+1, nodes.end()) ;
+    
+    for(int i=1 ; i<n ; i++)
+        parent[nodes[i]] = nodes[i-1] ;
+    
+    VI parent2(n) ;
+    
+    if(type==0) {
+        const int PART=5 ;
+        for(int i=1 ; i<n ; i+=PART)
+            swap(nodes[min(i+RG.rand()%PART,n-1)], nodes[min(i+RG.rand()%PART,n-1)]) ;
+    }
+    else if(type==1) {
+        VI part[2] ;
+        for(int i=1 ; i<n ; i++)
+            part[i&1].PB(nodes[i]) ;
+        
+        nodes.resize(1) ;
+        REP(i, 2)
+            FOREACH(it, part[i])
+                nodes.PB(*it) ;
+    }
+    else assert(false) ;
+    
+    for(int i=1 ; i<n ; i++)
+        parent2[nodes[i]] = nodes[i-1] ;
+    
+    line1.clear() ;
+    line2.clear() ;
+    for(int i=1 ; i<n ; i++) {
+        line1.PB(parent[i] +1) ;
+        line2.PB(parent2[i]+1) ;
+    }
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 
 struct {
@@ -107,6 +148,17 @@ void genRandomTestWithLongPath(int n, int pathSize1, int pathSize2) {
 void genRandomTestWithSqrtHeightAndWidth(int n) {
     test.n = n ;
     genTreesWithSqrtHeightAndWidth(n, test.line[0], test.line[1]) ;
+}
+
+void genRandomTestWithEqualPaths(int n) {
+    test.n = n ;
+    genTreeWithLongPath(n, n, test.line[0]) ;
+    test.line[1] = test.line[0] ;
+}
+
+void genRandomTestWithAlmostEqualPaths(int n, int type) {
+    test.n = n ;
+    genAlmostEqualPaths(n, test.line[0], test.line[1], type) ;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -130,13 +182,21 @@ int main()
         
         char nextLetter='a' ;
         FOREACH(n, N[t-1]) {
-            for(int g=0 ; g<GROUPSIZE ; g++) {
+            for(int g=0 ; g<PATH_RATIO_SIZE ; g++) {
                 genRandomTestWithLongPath(*n, (*n)*pathRatio1[g], (*n)*pathRatio2[g]) ;
                 ouput("test/0" + to_string(t) + "." + (nextLetter++) + ".in") ;
             }
         
             genRandomTestWithSqrtHeightAndWidth(*n) ;
             ouput("test/0" + to_string(t) + "." + (nextLetter++) + ".in") ;
+            
+            genRandomTestWithEqualPaths(*n) ;
+            ouput("test/0" + to_string(t) + "." + (nextLetter++) + ".in") ;
+            
+            REP(g, ALMOST_EQUAL_PATH_TYPES) {
+                genRandomTestWithAlmostEqualPaths(*n, g) ;
+                ouput("test/0" + to_string(t) + "." + (nextLetter++) + ".in") ;
+            }
         }
     }
 }
